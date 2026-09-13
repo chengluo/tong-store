6c215475f1e431468ef8d4b40b70c47d6a4b697c
Cheng Luo <home@chengs-mac-mini.lan>
2026-09-13 22:03:34 +0100

    Add middleware.ts for admin basic http authentication

---
+++ middleware.ts
@@ -0,0 +1,34 @@
+// middleware.ts (in your project root)
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  if (req.nextUrl.pathname.startsWith('/admin')) {
+    const basicAuth = req.headers.get('authorization');
+
+    if (basicAuth) {
+      const authValue = basicAuth.split(' ')[1];
+      const [user, pwd] = atob(authValue).split(':');
+
+      if (
+        user === process.env.ADMIN_USER &&
+        pwd === process.env.ADMIN_PASSWORD
+      ) {
+        return NextResponse.next();
+      }
+    }
+
+    return new NextResponse('Authentication required.', {
+      status: 401,
+      headers: {
+        'WWW-Authenticate': 'Basic realm="Komorebi Studio Admin"',
+      },
+    });
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/admin/:path*'],
+};
